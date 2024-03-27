4f4fcfe23eb3380b7cad87cebbb79520ed421509
zhukov <zhukov@etalon48.com>
2024-03-27 14:00:44 +0300

    test copilot

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
 export default function App() {
   return (
-    <h1 className='text-6xl'>App</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path="" element={}/>
+      
+      
+    </Routes>
+    </BrowserRouter>
   )
 }
