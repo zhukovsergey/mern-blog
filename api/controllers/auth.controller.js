8a925b37f9d76b8cc91ced4a4d8179b53dba7e52
zhukovsergey <zhukov@etalon48.com>
2024-04-02 12:29:14 +0300

    020424 12:30

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -36,15 +36,20 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
-      next(errorHandler(404, "Invalid User"));
+      return next(errorHandler(404, "Invalid User"));
     }
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
-      next(errorHandler(404, "Invalid Password"));
+      return next(errorHandler(404, "Invalid Password"));
     }
-    const token = jwt.sign(
-      {id: validUser._id},
-    )
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }
