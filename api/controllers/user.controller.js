682f431c6f0631cd882628ccfce70b328ffab90c
zhukovsergey <zhukov@etalon48.com>
2024-04-03 16:01:31 +0300

    05:00:00

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,65 @@
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import User from "../models/user.model.js";
+
 export const test = (req, res) => {
   res.json({ message: "Api working" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    return next(errorHandler(400, "You can not update your account."));
+  }
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(
+        errorHandler(400, "Password must be at least 6 characters long.")
+      );
+    }
+  }
+  req.body.password = bcryptjs.hashSync(req.body.password, 10);
+
+  if (req.body.username) {
+    if (req.body.username.length < 3 || req.body.username.length > 20) {
+      return next(
+        errorHandler(400, "Имя пользователя больше 3 и не больше 20 символов")
+      );
+    }
+    if (req.body.username.includes(" ")) {
+      return next(
+        errorHandler(400, "Имя пользователя не может содержать пробелы")
+      );
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(
+        errorHandler(400, "Имя пользователя должно быть в нижнем регистре")
+      );
+    }
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(
+          400,
+          "Имя пользователя должно содержать только латинские буквы и цифры"
+        )
+      );
+    }
+  }
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          profilePicture: req.body.profilePicture,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,11 +1,23 @@
-import { Button, TextInput } from "flowbite-react";
+import { Alert, Button, TextInput } from "flowbite-react";
 import { useSelector } from "react-redux";
 import { useState, useRef, useEffect } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadingProgress, setImageFileUploadingProgress] =
+    useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
   const filePickerRef = useRef(null);
   const handleImageChange = (event) => {
     const file = event.target.files[0];
@@ -20,8 +32,43 @@ export default function DashProfile() {
     }
   }, [imageFile]);
   const uploadImage = async () => {
-    
-  }
+    /*   service firebase.storage {
+      match /b/{bucket}/o {
+        match /{allPaths=**} {
+          allow read;
+          allow write: if 
+          request.resource.size < 2*1024*1024 &&
+          request.resource.contentType.matches('image/.*')
+        }
+      }
+    } */
+    setImageFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadingProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError(
+          "Не удалось загрузить файл, файл должен быть меньше 2 Мб и иметь расширение jpg, jpeg, png или gif"
+        );
+        setImageFileUploadingProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+        });
+      }
+    );
+  };
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Профиль</h1>
@@ -34,15 +81,44 @@ export default function DashProfile() {
           hidden
         />
         <div
-          className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+          className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
           onClick={() => filePickerRef.current.click()}
         >
+          {imageFileUploadingProgress && (
+            <CircularProgressbar
+              value={imageFileUploadingProgress || 0}
+              text={`${imageFileUploadingProgress}%  `}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62, 152, 199, ${
+                    imageFileUploadingProgress / 100
+                  })`,
+                },
+              }}
+            />
+          )}
           <img
             src={imageFileUrl || currentUser.profilePicture}
             alt="user"
-            className="rounded-full h-full w-full border-8 border-indigo-500 object-cover"
+            className={`rounded-full h-full w-full border-8 border-indigo-500 object-cover ${
+              imageFileUploadingProgress &&
+              imageFileUploadingProgress < 100 &&
+              "filter blur-sm"
+            }`}
           />
         </div>
+        {imageFileUploadError && (
+          <Alert color="failure">{imageFileUploadError}</Alert>
+        )}
+
         <TextInput
           type="text"
           id="username"
