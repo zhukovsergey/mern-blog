4aa78d688cb559737bf63481229dfdc81b431270
zhukovsergey <zhukov@etalon48.com>
2024-04-03 16:57:43 +0300

    05:07:41

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -7,57 +7,47 @@ export const test = (req, res) => {
 };
 
 export const updateUser = async (req, res, next) => {
-  if (req.user.id === req.params.id) {
-    return next(errorHandler(400, "You can not update your account."));
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update this user"));
   }
   if (req.body.password) {
     if (req.body.password.length < 6) {
-      return next(
-        errorHandler(400, "Password must be at least 6 characters long.")
-      );
+      return next(errorHandler(400, "Password must be at least 6 characters"));
     }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
   }
-  req.body.password = bcryptjs.hashSync(req.body.password, 10);
-
   if (req.body.username) {
-    if (req.body.username.length < 3 || req.body.username.length > 20) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
       return next(
-        errorHandler(400, "Имя пользователя больше 3 и не больше 20 символов")
+        errorHandler(400, "Username must be between 7 and 20 characters")
       );
     }
     if (req.body.username.includes(" ")) {
-      return next(
-        errorHandler(400, "Имя пользователя не может содержать пробелы")
-      );
+      return next(errorHandler(400, "Username cannot contain spaces"));
     }
     if (req.body.username !== req.body.username.toLowerCase()) {
-      return next(
-        errorHandler(400, "Имя пользователя должно быть в нижнем регистре")
-      );
+      return next(errorHandler(400, "Username must be lowercase"));
     }
     if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
       return next(
-        errorHandler(
-          400,
-          "Имя пользователя должно содержать только латинские буквы и цифры"
-        )
+        errorHandler(400, "Username can only contain letters and numbers")
       );
     }
   }
   try {
-    const updateUser = await User.findByIdAndUpdate(
+    const updatedUser = await User.findByIdAndUpdate(
       req.params.userId,
       {
         $set: {
           username: req.body.username,
           email: req.body.email,
-          password: req.body.password,
           profilePicture: req.body.profilePicture,
+          password: req.body.password,
         },
       },
       { new: true }
     );
-    const { password, ...rest } = updateUser._doc;
+    const { password, ...rest } = updatedUser._doc;
     res.status(200).json(rest);
   } catch (error) {
     next(error);
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -6,6 +6,6 @@ import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.get("/test", test);
-router.put("/update:userId", verifyToken, updateUser);
+router.put("/update/:userId", verifyToken, updateUser);
 
 export default router;
